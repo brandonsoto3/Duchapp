fe5ec6f7f25c4f0dc9cc7ec8c67276d59e64720b
brandonsoto3 <30421029+brandonsoto3@users.noreply.github.com>
2019-05-17 12:22:16 -0600

    Commit Inicial

---
+++ Duchapp/app/src/main/java/com/example/brandonsoto/duchapp/agua_descendente.java
@@ -0,0 +1,44 @@
+package com.example.brandonsoto.duchapp;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class agua_descendente extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.settings:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.estadisticas:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.play:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agua_descendente);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
